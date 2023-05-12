2ae3f673d856b40793762921fe5c3035594e0419
Cássio Castro <cassiocastro222@gmail.com>
2023-05-12 20:45:35 -0300

    Initial ajusts

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdint.h>
 
 #define SUCESS 0
+
+void mostra_frase(char *frase);
+
+int main(int argc, char **argv)
+{
+    printf("Hello World!\n");
+    return SUCESS;
+}
 
 /*! @brief Essa função tem como objetivo fazer um print do que você por dentro
 *   @param[in] Frase é a frase que irá ser imprimida no termial
@@ -9,10 +18,4 @@
 void mostra_frase(char *frase)
 {
     printf("%s", frase);
-}
-
-int main(int argc, char **argv)
-{
-    printf("Hello World!\n");
-    return SUCESS;
 }
