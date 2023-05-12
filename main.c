24ab7ff6397dc556d6717aa7e60164c121f21d42
Jonathan Drumond <jonathandrumond03@gmail.com>
2023-05-12 20:35:41 -0300

    Bin add

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+
 #define SUCESS 0
+
+/*! @brief Essa função tem como objetivo fazer um print do que você por dentro
+*   @param[in] Frase é a frase que irá ser imprimida no termial
+*   @return não existem parametros de retorno
+*/
+void mostra_frase(char *frase)
+{
+    printf("%s", frase);
+}
 
 int main(int argc, char **argv)
 {
+    printf("Hello World!\n");
     return SUCESS;
 }
